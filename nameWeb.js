b723c4ed8be9c213fbd739162f7e1adbafc730e3
Irina <104524265+Hogjiv@users.noreply.github.com>
2023-06-23 22:07:57 +0700

    working version. V1

--- nameWeb.js
+++ nameWeb.js
@@ -1,43 +1,78 @@
 const puppeteer = require("puppeteer");
-const pathToModels = "./images";
+const pathForSaveImage = "E:/03_3d lib/Library_Olimp/KITCHEN/Technika";
 const fs = require('fs');
 const axios = require('axios');
 
-const path = './models';
-fs.readdir(path, async (err, files) => {
+// find path in PC
+const pathOnPc = "E:/03_3d lib/Library_Olimp/KITCHEN/Technika";
+
+fs.readdir(pathOnPc, async (err, files) => {
   if (err) throw err;
-  for (const fileName of files) {
-    console.log(fileName, '111');
+    // read files according regExp  
+    console.log(files, "2");
+  const filteredFiles = files.filter(str => str.endsWith('.rar') || ('.zip'));
+  const replacedFiles = filteredFiles.map(file => file.replace(/\.rar$|\.zip$/, ""));
+  const modelName = replacedFiles.map(element => element.split(" "));
+  const modelCounter = modelName.reduce((count, element) => count + element.length, 0)
+  console.log(modelName, "total read" + modelCounter);
 
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-      userDataDir: "./tmp",
-    });
-    const page = await browser.newPage();
+  // open browser
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    userDataDir: "./tmp",
+  });
+  const page = await browser.newPage();
 
-    await page.goto(
-      "https://3ddd.ru/",
-      { waitUntil: "load", timeout: 0 }
-    );
+  await page.goto(
+    "https://3ddd.ru/",
+    { waitUntil: "load", timeout: 0 }
+  );
+  // put first value
+  await page.waitForSelector("#query_search");
+  
 
-    await page.waitForSelector("#query_search");
+  // create cycle
+  for (const model of modelName) {
+    console.log(model);
 
-    await page.type("#query_search", fileName);
+    for (const character of model) {
+      await page.type("#query_search", character);
+      await page.waitForTimeout(100);
+    }
+ 
+
+    // put value inside searchbar
     await page.keyboard.press("Enter");
+    // if any errors, continue run script
+    try {
+      await page.waitForSelector(".link", { timeout: 10000 });
+    } catch (err) {
+      await page.$eval("#query_search", (input) => (input.value = ''));
+      continue
+    }
 
-    await page.waitForSelector(".link", { timeout: 60000 });
+    // find correct css class in 3ddd
     const imageUrl = await page.$eval(".link img", (img) => img.src);
+
     console.log(imageUrl, 'step1');
 
+    // make rule for create new image name
     const rxName = /[^/]+$/;
     const imageNames = imageUrl.split(",");
+
+    
+
+    // make cycle for image name according regEx. 
     for (const imageName of imageNames) {
       const newName = imageName.match(rxName);
+
       console.log(newName, 'step2, got names of images');
 
-      const imagePath = pathToModels + '/' + newName[0];
+      // make correct name inside saving folder 
+      const imagePath = pathForSaveImage + '/' + newName[0];
 
+      // save image to correct path (imagePat)
       try {
         const response = await axios.get(imageName, { responseType: 'arraybuffer' });
         fs.writeFileSync(imagePath, response.data);
@@ -47,6 +82,11 @@ fs.readdir(path, async (err, files) => {
       }
     }
 
-    await browser.close();
-  }
+    // clear search string
+    await page.$eval("#query_search", (input) => (input.value = ''));
+    console.log('the string is empty')
+  } 
+
+  await browser.close();
+  console.log("Done!")
 });
